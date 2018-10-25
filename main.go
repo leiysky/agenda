d135fec7f807877832d20f66ae33f603f5be2019
leiysky <leiysky@outlook.com>
2018-10-25 22:16:59 +0800

    新增使用时对于配置文件的检查

--- main.go
+++ main.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"os"
+
 	"github.com/leiysky/agenda/commands"
 )
 
+func initConf() {
+	if _, err := os.Stat("/var/agenda"); err != nil {
+		if err := os.Mkdir("/var/agenda", 0); err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+			os.Exit(-1)
+		}
+	}
+	if _, err := os.Stat("/var/agenda/data.json"); err != nil {
+		dataFile, err := os.Create("/var/agenda/data.json")
+		os.Chmod("/var/agenda/data.json", 0666)
+		if err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+			os.Exit(-1)
+		}
+		dataFile.Write([]byte(`{
+			"collection": [],
+			"session": {}
+		}`))
+		dataFile.Close()
+		return
+	}
+	dataFile, err := os.OpenFile("/var/agenda/data.json", os.O_RDWR, 0)
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(-1)
+	}
+	dataFile.Close()
+}
+
 func main() {
+	initConf()
 	cmd := commands.NewAgendaCommand()
 	cmd.Execute()
 }
