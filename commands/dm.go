971a3ed3fc7f0ee8016f54998ab72da3fe139d4e
江新民 <1041965419@qq.com>
2018-10-27 15:21:42 +0800

    新增删除会议的字命令

--- commands/dm.go
+++ commands/dm.go
@@ -1,1 +1,40 @@
 package commands
+
+import (
+	"errors"
+
+	"github.com/leiysky/agenda/services/store"
+	"github.com/spf13/cobra"
+)
+
+type dmOptions struct {
+	refs []string
+}
+
+func newDeleteMeetingCommand() *cobra.Command {
+	var options dmOptions
+	cmd := &cobra.Command{
+		Use:   "dm TITLE",
+		Short: "delete a meeting",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("requires at least 1 arguments")
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			options.refs = args
+			return runDeleteMeetingsCommand(options)
+		},
+	}
+
+	return cmd
+}
+
+func runDeleteMeetingsCommand(opts dmOptions) error {
+	if !store.IsLoggedIn() {
+		return errors.New("not authenticated")
+	}
+	meetingName := opts.refs[0]
+	return store.DeleteMeetingByName(meetingName)
+}
